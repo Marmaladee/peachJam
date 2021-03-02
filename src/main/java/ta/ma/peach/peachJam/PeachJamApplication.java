3aacaf9263e52c30d7de89360373a1df1ef40bb5
Marmaladee <tamayura@naver.com>
2021-03-02 13:42:36 +0900

    Initial commit

---
+++ src/main/java/ta/ma/peach/peachJam/PeachJamApplication.java
@@ -0,0 +1,13 @@
+package ta.ma.peach.peachJam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachJamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeachJamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ta/ma/peach/peachJam/PeachJamApplicationTests.java
@@ -0,0 +1,13 @@
+package ta.ma.peach.peachJam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachJamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
